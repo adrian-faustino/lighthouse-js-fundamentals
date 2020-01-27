121056af6bccdb543a6b860b717a512657c46058
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-01-27 18:52:17 +0000

    Tinkered with whichSchool functions and conditionals

--- tinkering.js
+++ tinkering.js
@@ -5,3 +5,17 @@ function multiply(a,b) {
 const result = multiply(2, 4);
 
 console.log(result);
+
+
+const whichSchool  = function (age) {
+  let school = "";
+  if (age < 13) {
+    school = "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    school = "Secondary School";
+  } else school = "Lighthouse Labs";
+
+  return school;
+}
+
+console.log(whichSchool(13));
