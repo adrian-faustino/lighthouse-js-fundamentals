332f8bcf26706569a4211b88ea024805bbeddaaa
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-01-28 18:08:35 +0000

    Changed for loop to while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
-  console.log(packingList[i]);
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+let counter = 0;
+while (counter < packingList.length) {
+  console.log(packingList[counter]);
+  counter++;
 }
+
